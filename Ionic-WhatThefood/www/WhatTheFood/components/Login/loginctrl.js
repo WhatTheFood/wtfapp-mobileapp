b2857e8c981b0841e4c6ad93515209b8996eb708
Nicolas Form <nicolas.form@gmail.com>
2015-02-14 18:04:30 +0100

    Add FB login

--- Ionic-WhatThefood/www/WhatTheFood/components/Login/loginctrl.js
+++ Ionic-WhatThefood/www/WhatTheFood/components/Login/loginctrl.js
@@ -2,5 +2,23 @@ wtf.controller('loginctrl', ['$scope','$state', function($scope, $state) {
     
     $scope.gohome = function(){
         $state.go('wtf.rulist');
-    }
+	}
+	
+	$scope.fbLogin = function() {
+		openFB.login(
+		function(response) {
+			if (response.status === 'connected') {
+				console.log('Login Facebook reussie !');
+				openFB.api({path: '/me/friends',
+					success: function(nb) {
+						console.log(nb);
+					},
+				error: function() {alert('Impossible de récupérer la liste d\'amis');}});
+				$scope.closeLogin();
+				} else {
+				alert('Login Facebook impossible...');
+			}
+		},
+		{scope: 'email,user_friends'});
+	}
 }]);
