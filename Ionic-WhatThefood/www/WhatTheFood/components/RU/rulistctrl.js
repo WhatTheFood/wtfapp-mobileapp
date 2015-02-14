4eaa386f460e3b13bfb40a4f08656d151bd6c6ec
Rony Cohen <cohen.rony.pro@gmail.com>
2015-02-14 15:41:12 +0100

    List RU KM

--- Ionic-WhatThefood/www/WhatTheFood/components/RU/rulistctrl.js
+++ Ionic-WhatThefood/www/WhatTheFood/components/RU/rulistctrl.js
@@ -15,6 +15,19 @@ wtf.controller('rulistctrl', ['$scope', 'rulistservice', function($scope, rulist
     rulistservice.getrulist().then(function(result){
         console.log(result);
         $scope.rulist = result.data;
+        rulistservice.getPosition().then(function(coord){
+            console.log(coord);
+            rulistservice.getrulist(coord.latitude,coord.longitude).then(function(result){
+                var data2 =  result.data.map(function(val){
+                    console.log(val);
+                    val.distance = Math.round(val.distance * 6378.137);
+                    return val ;
+                });
+
+                $scope.rulist = data2;
+                $scope.geomsg = "Voici les RU près de vous";
+            });
+        });
     });
 
 }]);
--- Ionic-WhatThefood/www/js/app.js
+++ Ionic-WhatThefood/www/js/app.js
@@ -7,98 +7,104 @@
 // 'starter.controllers' is found in controllers.js
 var wtf = angular.module('whatthefood', ['ionic', 'whatthefood.controllers', 'whatthefood.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function($ionicPlatform) {
 
-.config(function($stateProvider, $urlRouterProvider) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('wtf', {
-    url: "/wtf",
-    abstract: true,
-    templateUrl: "WhatTheFood/shared/basecontent.html"
-  })
+        
 
-  // Each tab has its own nav history stack:
 
-  .state('wtf.rulist', {
-    url: '/rulist',
-    views: {
-      'menuContent': {
-        templateUrl: 'WhatTheFood/components/RU/rulistview.html',
-        controller: 'rulistctrl'
-      }
-    }
-  })
-
-  .state('wtf.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('wtf.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
     })
 
-  .state('wtf.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('wtf.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
+    .config(function($stateProvider, $urlRouterProvider) {
 
-  .state('wtf.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/wtf/rulist');
+            // setup an abstract state for the tabs directive
+            .state('wtf', {
+                url: "/wtf",
+                abstract: true,
+                templateUrl: "WhatTheFood/shared/basecontent.html"
+            })
 
-});
+            // Each tab has its own nav history stack:
+
+            .state('wtf.rulist', {
+                url: '/rulist',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'WhatTheFood/components/RU/rulistview.html',
+                        controller: 'rulistctrl'
+                    }
+                }
+            })
+
+            .state('wtf.chats', {
+                url: '/chats',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'templates/tab-chats.html',
+                        controller: 'ChatsCtrl'
+                    }
+                }
+            })
+            .state('wtf.chat-detail', {
+                url: '/chats/:chatId',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'templates/chat-detail.html',
+                        controller: 'ChatDetailCtrl'
+                    }
+                }
+            })
+
+            .state('wtf.friends', {
+                url: '/friends',
+                views: {
+                    'tab-friends': {
+                        templateUrl: 'templates/tab-friends.html',
+                        controller: 'FriendsCtrl'
+                    }
+                }
+            })
+            .state('wtf.friend-detail', {
+                url: '/friend/:friendId',
+                views: {
+                    'tab-friends': {
+                        templateUrl: 'templates/friend-detail.html',
+                        controller: 'FriendDetailCtrl'
+                    }
+                }
+            })
+
+            .state('wtf.account', {
+                url: '/account',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/tab-account.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/wtf/rulist');
+
+    });
 
 
