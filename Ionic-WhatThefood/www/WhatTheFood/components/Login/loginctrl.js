14aba4dac178e5da8c304a69b252faabae00f673
loic lefloch <l.lefloch.91@gmail.com>
2015-02-15 21:04:06 +0100

    Login loading

--- Ionic-WhatThefood/www/WhatTheFood/components/Login/loginctrl.js
+++ Ionic-WhatThefood/www/WhatTheFood/components/Login/loginctrl.js
@@ -1,12 +1,20 @@
-wtf.controller('loginctrl', ['$scope','$state','$http','loginservice','$cordovaOauth','$localStorage',
-    function($scope, $state, $http, loginservice, $cordovaOauth, $localStorage ) {
+wtf.controller('loginctrl', ['$scope','$state','$http','loginservice','$cordovaOauth','$localStorage', '$ionicLoading',
+
+    function($scope, $state, $http, loginservice, $cordovaOauth, $localStorage, $ionicLoading ) {
 
         $scope.gohome = function(){
             $state.go('wtf.rulist');
         };
 
         $scope.fblogin = function()  {
+
+            $ionicLoading.show({
+                template: '<i class="button-icon icon ion-loading-a"></i><br> Veuillez patienter.'
+            });
+
+
             loginservice.loginfb().then(function(user) {
+                    $ionicLoading.hide()
                     console.log("Token API : "+user);
                     if(user == "error") {
                         alert("Erreur login");
