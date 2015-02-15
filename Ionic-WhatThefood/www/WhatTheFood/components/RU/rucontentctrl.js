dd44e5e4f6d64aad313bfc6784352afd77f6740e
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-02-15 11:41:47 +0100

    Fixing horaires for RU view.

--- Ionic-WhatThefood/www/WhatTheFood/components/RU/rucontentctrl.js
+++ Ionic-WhatThefood/www/WhatTheFood/components/RU/rucontentctrl.js
@@ -2,12 +2,15 @@
  * Created by Rony on 14/02/2015.
  */
 
-wtf.controller('rucontentctrl', ['$scope', '$stateParams', 'rulistservice', function($scope, $stateParams, rulistservice) {
+wtf.controller('rucontentctrl', ['$scope', '$sce', '$stateParams', 'rulistservice', function($scope, $sce, $stateParams, rulistservice) {
     console.log($stateParams);
     console.log(rulistservice.restaurants);
     var restaurant = rulistservice.restaurants.filter(function(restaurant) {
         return restaurant.id == $stateParams.ruId;
     })
     $scope.ru = restaurant[0];
+    console.log("operational hoursssss : ");
+    //$scope.ru.operationalhours = 
+    $scope.operationalhours = $sce.trustAsHtml(restaurant[0].operationalhours.replace(/  /g, "<br />"));
     console.log($scope.ru);
 }]);
