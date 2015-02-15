5794ed95ffcd61748784ba43525bcc64973ee260
François Dupayrat <fradow@gmail.com>
2015-02-15 17:05:32 +0100

    Add waiting times in ruqueue, rename restaurants to rulist

--- Ionic-WhatThefood/www/WhatTheFood/components/RU/ruqueuectrl.js
+++ Ionic-WhatThefood/www/WhatTheFood/components/RU/ruqueuectrl.js
@@ -1,6 +1,9 @@
 wtf.controller('ruqueuectrl', ['$scope', '$state', '$stateParams', 'rulistservice', 'loginservice', function($scope, $state, $stateParams, rulistservice, loginservice) {
-   $scope.restaurants = rulistservice.restaurants;
+   $scope.rulist = rulistservice.restaurants;
    console.log("state params: ", $stateParams.ruId);
-   $scope.modelRu = $scope.restaurants[0];
-   $scope.preselectruId = $stateParams.ruId === undefined ? $scope.restaurants[0].id : $stateParams.ruId;
+   $scope.modelRu = $scope.rulist[0];
+   $scope.preselectruId = $stateParams.ruId === undefined ? $scope.rulist[0].id : $stateParams.ruId;
+   $scope.waitingTimes = [{title:'Je rentre dans le RU en moonwalk', img:'img/clock_green.png'}, 
+   {title:'Juste le temps de rêvasser', img:'img/clock_orange.png'}, 
+   {title:'On peut y réviser ses partiels', img:'img/clock_red.png'}];
 }]);
