00b19d3d0b2eb5730e1f8ec81faf04d01258a30f
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-02-15 18:00:23 +0100

    Cleaning commented bloc.

--- Ionic-WhatThefood/www/WhatTheFood/components/Login/loginctrl.js
+++ Ionic-WhatThefood/www/WhatTheFood/components/Login/loginctrl.js
@@ -16,16 +16,4 @@ wtf.controller('loginctrl', ['$scope','$state','$http','loginservice','$cordovaO
                     }
                 }
             )};
-
-/*        $scope.login = function() {
-            $cordovaOauth.facebook("576723975798203", ["email", "read_stream", "user_website", "user_location", "user_relationships"]).then(function(result) {
-                $localStorage.accessToken = result.access_token;
-                console.log(result);
-                //$location.path("/profile");
-            }, function(error) {
-                alert("There was a problem signing in!  See the console for logs");
-                console.log(error);
-            });
-        };*/
-
     }]);
