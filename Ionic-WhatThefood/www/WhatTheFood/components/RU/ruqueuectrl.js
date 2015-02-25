03c2bddbc77a520eb161a64a288ab3e0a1328281
euhmeuh <merdelin.mortetombe@laposte.net>
2015-02-26 00:25:47 +0100

    Send queue state to the server.

--- Ionic-WhatThefood/www/WhatTheFood/components/RU/ruqueuectrl.js
+++ Ionic-WhatThefood/www/WhatTheFood/components/RU/ruqueuectrl.js
@@ -1,9 +1,48 @@
-wtf.controller('ruqueuectrl', ['$scope', '$state', '$stateParams', 'rulistservice', 'loginservice', function($scope, $state, $stateParams, rulistservice, loginservice) {
-   $scope.rulist = rulistservice.restaurants;
-   console.log("state params: ", $stateParams.ruId);
-   $scope.modelRu = $scope.rulist[0];
-   $scope.preselectruId = $stateParams.ruId === undefined ? $scope.rulist[0].id : $stateParams.ruId;
-   $scope.waitingTimes = [{title:'Je rentre dans le RU en moonwalk', img:'img/clock_green.png'}, 
-   {title:'Juste le temps de rêvasser', img:'img/clock_orange.png'}, 
-   {title:'On peut y réviser ses partiels', img:'img/clock_red.png'}];
+wtf.controller('ruqueuectrl', ['$scope', '$state', '$stateParams', '$http', 'rulistservice', 'loginservice', function($scope, $state, $stateParams, $http, rulistservice, loginservice) {
+
+    /* populate combobox */
+    $scope.rulist = rulistservice.restaurants;
+    $scope.currentRu = $scope.rulist[0];
+
+    /* waiting times */
+    $scope.waitingTimes = [
+        {index: 0, title:'Je rentre dans le RU en moonwalk', img:'img/clock_green.png'},
+        {index: 1, title:'Juste le temps de rêvasser', img:'img/clock_orange.png'},
+        {index: 2, title:'On peut y réviser ses partiels', img:'img/clock_red.png'}
+    ];
+
+    $scope.sendVote = function(index) {
+        console.log(index);
+
+        /* don't execute if there is no token */
+        //if(loginservice.gettoken() == "") return "not connected";
+
+        if($scope.currentRu == null) return "error";
+
+        var req = {
+            method: 'POST',
+            dataType: "json",
+            url: loginservice.getServerAPI()+'/restaurants/'+$scope.currentRu.id+'/queue/votes',
+            data: '{"timeSlotIndex": '+ index +'}',
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization" : "Bearer "+loginservice.gettoken()
+            }
+        };
+
+        console.log(req);
+
+        $http(req)
+        .success(function (data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            return data;
+        })
+        .error(function (data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log(data);
+            return "error";
+        });
+    };
 }]);
