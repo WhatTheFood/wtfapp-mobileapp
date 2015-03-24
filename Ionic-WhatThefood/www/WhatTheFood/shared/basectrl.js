67a1d5676ce5d65959a632fcfabd5f469572a7ab
Nicolas Form <nicolas.form@gmail.com>
2015-03-24 19:44:46 +0100

    Clear left menu + open facebook page

--- Ionic-WhatThefood/www/WhatTheFood/shared/basectrl.js
+++ Ionic-WhatThefood/www/WhatTheFood/shared/basectrl.js
@@ -1,9 +1,12 @@
-wtf.controller('basectrl', ['$scope', '$state', function($scope, $state) {
+wtf.controller('basectrl', ['$scope', '$state', '$cordovaInAppBrowser', function($scope, $state, $cordovaInAppBrowser) {
     $scope.ru = undefined;
     $scope.showQueue = function () {
         var ruId = $scope.ru === undefined ? undefined : $scope.ru.id;
         $state.go('wtf.ruqueue', {ruId: ruId});
     };
+    $scope.openExternal = function(url) {
+        $cordovaInAppBrowser.open(url, 'system')
+    }
     $scope.setContextRu = function(ru) {
         $scope.ru = ru;
     }
