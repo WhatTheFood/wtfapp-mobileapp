d1aee25ee3d5707bb89a11e477e071a2cec24c6a
Adrien Giboire <adrien.giboire@gmail.com>
2015-05-02 17:50:51 +0200

    fix: app dependency

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 var wtf = angular.module('whatthefood', ['ionic', 'whatthefood.controllers', 'whatthefood.services', 'ngCordova', 'ngStorage'])
 
-.run(['$ionicConfigProvider', function($ionicPlatform) {
+.run(['$ionicPlatform', function($ionicPlatform) {
   $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
