59cfd2fb8785114604e3241015396ca8a095599a
fel <fxkuntz@gmail.com>
2017-07-22 16:29:45 +0200

    set link to new demo link

--- www/WhatTheFood/components/Login/loginservice.js
+++ www/WhatTheFood/components/Login/loginservice.js
@@ -12,7 +12,7 @@ wtf.factory('loginservice', ['$http', '$q', '$localStorage', '$sessionStorage',
 
         // Debug handy
         var serverAPI = "//localhost:5000/api";
-        serverAPI = "https://m.whatthefoodapp.fr/api"; // prod
+        serverAPI = "https://wtf.weit.fr/api"; // prod
 
         var factory = {
 
